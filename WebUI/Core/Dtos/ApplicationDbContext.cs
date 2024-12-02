ce541c90f372eb6b9bcc5c04664814588a7e4b9c
phantom99a1 <viendanho99a1@gmail.com>
2024-12-02 22:29:32 +0700

    Add new project

---
+++ WebUI/Core/Dtos/ApplicationDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Core.Entities;
+
+namespace WebUI.Core.Dtos
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Log> Logs { get; set; }
+        DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(e =>
+            {
+                e.ToTable("Users");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(e =>
+            {
+                e.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(e =>
+            {
+                e.ToTable("UserLogins");
+            });
+
+            builder.Entity<IdentityUserToken<string>>(e =>
+            {
+                e.ToTable("UserTokens");
+            });
+
+            builder.Entity<IdentityRole>(e =>
+            {
+                e.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(e =>
+            {
+                e.ToTable("RoleClaims");
+            });
+
+            builder.Entity<IdentityUserRole<string>>(e =>
+            {
+                e.ToTable("UserRoles");
+            });
+        }
+    }
+}
