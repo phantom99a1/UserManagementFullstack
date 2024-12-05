2f31baeb90e59128538f202aa847b72d8dab44d1
phantom99a1 <viendanho99a1@gmail.com>
2024-12-05 22:48:45 +0700

    Update project

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9331cd3d63fe1d2eed8c96d55515462eae922f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2938f34877eb43b1826eb62838d142230b8f576")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
